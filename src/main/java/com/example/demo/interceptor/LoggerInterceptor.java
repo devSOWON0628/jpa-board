655b8212e9c067c6e14d8787b7e0b16b9fe44b21
parksowon <psw@gsitm.com>
2021-03-10 17:49:37 +0900

    만약! 쿠키가 없다면.. 다른 화면으로 리다이렉트!

--- src/main/java/com/example/demo/interceptor/LoggerInterceptor.java
+++ src/main/java/com/example/demo/interceptor/LoggerInterceptor.java
@@ -1,5 +1,6 @@
 package com.example.demo.interceptor;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,17 +15,34 @@ public class LoggerInterceptor extends HandlerInterceptorAdapter  {
 
 	@Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		logger.info("Interceptor > preHandle");
+		// 컨트롤러(즉 RequestMapping이 선언된 메서드 진입) 실행 직전에 동작.
+		Cookie[] cookies = request.getCookies() ;
+		if(cookies == null) {
+			response.sendRedirect("https://gw.gsitm.com/");
+			return false;
+		}
+		logger.info("Interceptor > preHandle cookie : "+cookies[0].getValue());
 		return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    	// 컨트롤러 진입 후 view가 랜더링 되기 전 수행이 됩니다.
     	logger.info("Interceptor > postHandle");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object object, Exception arg3) throws Exception {
-    	logger.info("Interceptor > afterCompletion" );
+    	// 컨트롤러 진입 후 view가 정상적으로 랜더링 된 후 제일 마지막에 실행이 되는 메서드입니다.
+    	logger.info("Interceptor > afterCompletion");
     }
+
+	@Override
+	public void afterConcurrentHandlingStarted(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		// Servlet 3.0 부터 비동기 요청이 가능해짐에 따라 비동기 요청 시 PostHandle와 afterCompletion메서드를 수행하지 않고 이 메서드를 수행하게 됩니다. (Spring에서 제공함)
+		super.afterConcurrentHandlingStarted(request, response, handler);
+		logger.info("Interceptor > afterConcurrentHandlingStarted");
+	}
+    
+    
 }
